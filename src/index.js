6a2329230042cfe838d1cf26f5b2e3370b626831
Abhay <abhayrnair2005@gmail.com>
2024-11-11 10:09:39 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'; // Import applyMiddleware
+import {thunk} from 'redux-thunk'; // Import thunk middleware
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+import { reducers } from './reducers'; // Import your root reducer
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
+
+
+// Create the Redux store with thunk middleware
+const store = createStore(reducers, applyMiddleware(thunk)); // Apply middleware
+
+// Create a theme instance
+const theme = createTheme({
+  spacing: (factor) => `${0.25 * factor}rem`, //
+  palette: {
+    primary: {
+      main: '#1976d2', 
+    },
+    secondary: {
+      main: '#dc004e', 
+    },
+  },
+  
+});
+
+// Get the root element
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Render the application
+root.render(
+  <Provider store={store}> 
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </Provider>
+);
